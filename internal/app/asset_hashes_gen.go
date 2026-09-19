ed8395f279f991eb1a146542121bb9e594362b47
Jordi Brouwer <jordi.brouwer@gmail.com>
2026-09-19 23:01:28 +0200

    release v1.11.8: settings tiles and quieter glass, held back from the modal

--- internal/app/asset_hashes_gen.go
+++ internal/app/asset_hashes_gen.go
@@ -237,7 +237,7 @@ var precomputedAssetHashes = map[string]string{
 	"js/visual-settings.js":                                  "c373da16066f",
 	"js/weather.js":                                          "df506456219f",
 	"js/whats-new-modal.js":                                  "9096e8453b83",
-	"js/whats-new-stub.js":                                   "f2e53e21e4e4",
+	"js/whats-new-stub.js":                                   "708f225026c5",
 	"js/widgets-tutorial.js":                                 "4bd39fee3746",
 	"js/write-api.js":                                        "25afcd408905",
 }
