fbf03bce702df1488c28f2f4dfbb93be801fbfd9
Jordi Brouwer <jordi.brouwer@gmail.com>
2026-09-23 16:21:42 +0200

    docs+i18n: help on the certificate webhook and where backups live, manual and changelog for v1.13.2

--- internal/app/asset_hashes_gen.go
+++ internal/app/asset_hashes_gen.go
@@ -245,7 +245,7 @@ var precomputedAssetHashes = map[string]string{
 	"js/visual-settings.js":                                  "c373da16066f",
 	"js/weather.js":                                          "1e7e9e115447",
 	"js/whats-new-modal.js":                                  "9096e8453b83",
-	"js/whats-new-stub.js":                                   "9c34cd9db1b4",
+	"js/whats-new-stub.js":                                   "455d14494d7d",
 	"js/widgets-tutorial.js":                                 "4bd39fee3746",
 	"js/write-api.js":                                        "25afcd408905",
 }
